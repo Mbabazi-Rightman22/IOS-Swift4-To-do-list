c4288055d3015173a910f50866a62809e1d3fe74
Aimable <niyoai@yahoo.fr>
2017-10-31 22:14:05 +0000

    Update SecondViewController.swift

--- SecondViewController.swift
+++ SecondViewController.swift
@@ -10,40 +10,24 @@ import UIKit
 
 class SecondViewController: UIViewController, UITableViewDelegate {
 
-    
     @IBOutlet var itemTextField: UITextField!
     
     @IBAction func add(_ sender: Any) {
-        
         let  itemsObject = UserDefaults.standard.object(forKey: "items")
-        
         var items:[String]
-        
-       
-        
         if let tempItems = itemsObject as? [String] {
             
             items = tempItems
-            
             items.append(itemTextField.text!)
-            
             print(items)
-            
         }else {
-            
              items = [itemTextField.text!]
         }
-        
         UserDefaults.standard.set(items, forKey: "items")
-        
         itemTextField.text = ""
-        
     }
-    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
         self.view.endEditing(true)
-        
     }
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         
@@ -54,13 +38,8 @@ class SecondViewController: UIViewController, UITableViewDelegate {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-
 }
-
