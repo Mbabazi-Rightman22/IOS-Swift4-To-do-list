a63b789b40d8f6efaf36f47b59f3d8cd03b5c79a
Aimable <niyoai@yahoo.fr>
2017-10-31 22:10:53 +0000

    Update FirstViewController.swift

--- FirstViewController.swift
+++ FirstViewController.swift
@@ -15,28 +15,19 @@ class FirstViewController: UIViewController,UITableViewDataSource,UITableViewDel
     var items:[String] = []
     
     internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
        
         func refreshUI() {
             DispatchQueue.main.async{
                 tableView.reloadData() }
-            
         }
          return items.count
         
-    }
-    
-    
-    
+    }  
     internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
-        
-        
         cell.textLabel?.text = (items[indexPath.row] )
-        
         return cell
-        
     }
 
     override func viewDidLoad() {
@@ -44,41 +35,25 @@ class FirstViewController: UIViewController,UITableViewDataSource,UITableViewDel
         // Do any additional setup after loading the view, typically from a nib.
 
     }
-    
     override func viewDidAppear(_ animated: Bool) {
         
         var itemsObject = UserDefaults.standard.object(forKey: "items")
-        
         if let tempItems = itemsObject as? [String] {
-            
             itemsObject = tempItems
-            
         }
         table.reloadData()
-        
     }
-    
-   
-
-    
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         
         if editingStyle == UITableViewCellEditingStyle.delete {
-            
             items.remove(at: indexPath.row)
-            
             table.reloadData()
-            
             UserDefaults.standard.set(items, forKey: "items")
-            
         }
 
         func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
    }
-
 }
